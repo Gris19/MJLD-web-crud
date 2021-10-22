47899aa3750a5594e5f820de06ccf563354549e7
Majo <majo74250@gmail.com>
2021-10-21 19:30:05 -0500

    mi sitio web

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCmOxembReVASOCLestw-9AB0_60RR1WAc",
+  authDomain: "mjld-web-crud-dfddc.firebaseapp.com",
+  projectId: "mjld-web-crud-dfddc",
+  storageBucket: "mjld-web-crud-dfddc.appspot.com",
+  messagingSenderId: "990810210560",
+  appId: "1:990810210560:web:e5384be02edafa59d673e5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
